b6351dbbdba30cd40bd6f060a36c653addd0f488
Michael Gharbi <gharbi@mit.edu>
2018-07-30 11:52:24 -0400

    adds python pipe gen

--- apps/json_rnd_pipeline/generate.py
+++ apps/json_rnd_pipeline/generate.py
@@ -3,19 +3,25 @@
 
 import argparse
 import os
+import copy
 import subprocess
+import time
+
 from multiprocessing import Process, Queue, Pool, JoinableQueue
+
 
 class GeneratorParams(object):
   """Parameters driving the generator"""
   def __init__(self, hl_target, hl_seed, pipe_seed, stages,
-               dropout, beam):
+               dropout, beam, timeout):
     self.hl_target = hl_target
     self.hl_seed = hl_seed
     self.pipeline_seed = pipe_seed
     self.stages = stages
     self.dropout = dropout
     self.beam = beam
+
+    self.timeout = timeout
 
   def __repr__(self):
     return "GeneratorParams<sched{},pipe{},stages{}>".format(
@@ -31,43 +37,62 @@ class GeneratorParams(object):
       "HL_BEAM_SIZE": str(self.beam),
     }
 
+def get_pipeline_env(params):
+  """Preserve user env, and add pipeline-relevant vars."""
+  env = copy.deepcopy(os.environ)
+  env.update(params.env())
+  return env
+
 
 def build_one(q):
   while True:
     params = q.get(block=True, timeout=None)
-    print("Process", os.getpid(), params)
-    subprocess.run(["make", "build"], env=params.env())
-    # TODO: timeout on compile
-    # ret = subprocess.run(["./test.sh"], env=params.env())
+    env = get_pipeline_env(params)
+
+    start = time.time()
+    subprocess.check_output(["make", "build"], env=env, timeout=params.timeout)
+    elapsed = time.time() - start
+
     q.task_done()
 
-def run_one(q):
-  pass
+    print("pid {} {}, compiled in {}s".format(os.getpid(), params, elapsed))
+
 
 def main(args):
-  # print("Building shared binaries")
-  subprocess.run(["make", "build"])
+  print("Building shared binaries")
+  subprocess.run(["make", "build_shared"])
 
   #TODO(mgharbi): add metadata about the pipe gen
   q = JoinableQueue()
   pool = Pool(1, build_one, (q, ))
 
   # Distributed build
+  print("\nBuilding pipelines")
   for b in range(args.batches):
     for p in range(args.pipelines):
-      # number of stages for that pipeline
-      stages = (p % 30) + 10
-      params = GeneratorParams(
-        args.hl_target, "root", p, stages, args.dropout, args.beam_size)
-      q.put(params, block=True)
-      for s in range(args.schedules):
+      stages = (p % 30) + 10  # number of stages for that pipeline
+      for s in ["root"] + list(range(args.schedules)):
         params = GeneratorParams(
-          args.hl_target, s, p, stages, args.dropout, args.beam_size)
+          args.hl_target, s, p, stages, args.dropout,
+          args.beam_size, args.timeout)
         q.put(params, block=True)
-  print("Waiting for queue completion")
   q.join()
 
   # Sequential benchmarking
+  print("\nBenchmarking pipelines")
+  for b in range(args.batches):
+    for p in range(args.pipelines):
+      stages = (p % 30) + 10  # number of stages for that pipeline
+      for s in ["root"] + list(range(args.schedules)):
+        params = GeneratorParams(
+          args.hl_target, s, p, stages, args.dropout,
+          args.beam_size, args.timeout)
+        env = get_pipeline_env(params)
+        env["HL_NUM_THREASD"] = str(os.cpu_count())
+        subprocess.run(["make", "bench"], env=env, timeout=params.timeout)  # numactl --cpunodebind=0 make bench
+
+  # Gather dataset
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
@@ -78,5 +103,6 @@ if __name__ == "__main__":
   parser.add_argument("--hl_target", type=str, default="host-new_autoscheduler")
   parser.add_argument("--dropout", type=int, default=50)
   parser.add_argument("--beam_size", type=int, default=2)
+  parser.add_argument("--timeout", type=float, default=10.0, help="in seconds")
   args = parser.parse_args()
   main(args)
